9c7f0ebc33b9d6f14602bbd319bbb8c13b8df728
A Star Vlog <admin@astarvlog.com>
2020-12-27 16:03:52 +0800

    first

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-layout view="lHr Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+        <q-btn
+          flat
+          dense
+          round
+          class="q-ml-md"
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+      </q-toolbar>
+      <q-tabs align="left">
+        <q-route-tab v-for="(each,i) in essentialLinks" :key="i" :to="each.link" :label="each.title" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      width="200"
+      bordered
+      side="right"
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Home',
+    caption: '',
+    icon: 'home',
+    link: '/'
+  },
+  {
+    title: 'Dashboard',
+    caption: '',
+    icon: 'dashboard',
+    link: 'dashboard'
+  },
+  {
+    title: 'Login',
+    caption: '',
+    icon: 'login',
+    link: 'login'
+  },
+  {
+    title: 'Register',
+    caption: '',
+    icon: 'how_to_reg',
+    link: 'register'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Register.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="q-pa-lg" style="max-width: 400px">
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="username"
+        label="Your username *"
+        hint="No space"
+        lazy-rules
+        :rules="[ val => val && val.length > 3 || 'Please type something']"
+      />
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="Your password *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your password'
+        ]"
+      />
+      <q-input
+        filled
+        type="password"
+        v-model="confirmPassword"
+        label="Confirm password *"
+        :rules="[
+          val => val !== null && val !== '' || 'Please confirm your password'
+        ]"
+      />
+      <div class="text-caption text-grey-7 text-italic">
+        By clicking here, you registering, you accept the rules....
+      </div>
+      <q-toggle v-model="accept" label="I accept the license and terms" />
+      <div>
+        <q-btn label="Submit" type="submit" color="negative" /> 
+        <q-btn label="Reset" type="reset" color="blue" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      username: null,
+      password: null,
+      confirmPassword: null,
+      accept: false
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'You need to accept the license and terms first'
+        })
+      } else {
+        if(this.password==this.confirmPassword){
+          this.$q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'fa fa-bath',
+            message: 'Submitted'
+          })
+        }else{
+          this.$q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'Your password didn\'t match'
+          })
+        }
+      }
+    },
+
+    onReset () {
+      this.username = null
+      this.password = null
+      this.confirmPassword = null
+      this.accept = false
+    }
+  }
+}
+</script>
